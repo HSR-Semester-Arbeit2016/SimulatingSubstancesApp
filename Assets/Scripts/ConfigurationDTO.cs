dc56165cd02c25480773147c713ceca41c713388
Roberto Cuervo <rcuervoa@hsr.ch>
2016-11-12 23:18:47 +0100

    Extendend ConfigurationDTO class
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

--- Assets/Scripts/ConfigurationDTO.cs
+++ Assets/Scripts/ConfigurationDTO.cs
@@ -1,26 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public class ConfigurationDTO : MonoBehaviour
 {
 	private static ConfigurationDTO instance;
+	private static string configurationName = "";
 
-	public float BlurLevel { get; set; }
+	public string Name { get { return configurationName; } set { configurationName = value; } }
 
-	public float TunnelLevel { get; set; }
+	private static float blurLevel = 0.0f;
 
-	public float DelayLevel { get; set; }
+	/// <summary>
+	/// Blur level from 0 to 15
+	/// </summary>
+	/// <value>The blur level.</value>
+	public float BlurLevel { get { return blurLevel; } set { blurLevel = value; } }
+
+	private static float tunnelLevel = 0.0f;
+
+	/// <summary>
+	/// Tunnel level from 0 to 25
+	/// </summary>
+	/// <value>The tunnel level.</value>
+	public float TunnelLevel { get { return tunnelLevel; } set { tunnelLevel = value; } }
+
+
+	private static int delayValue = 0;
+
+	/// <summary>
+	/// Boolean wrapper. Gets or sets the delay. 0 = off, 1 = on
+	/// </summary>
+	/// <value>The delay.</value>
+	public int Delay { get { return delayValue; } set { delayValue = value; } }
+
+
+	private  static int motionBlur = 0;
+
+	/// <summary>
+	/// Boolean wrapper. Gets or sets the motion blur. 0 = off, 1 = on.
+	/// </summary>
+	/// <value>The motion blur.</value>
+	public int MotionBlur { get { return motionBlur; } set { motionBlur = value; } }
+
+
+	private static int redColor = 0;
+
+	/// <summary>
+	/// Boolean wrapper. Gets or sets the color of the red. 0 = off, 1 = on.
+	/// </summary>
+	/// <value>The color of the red.</value>
+	public int RedColor { get { return redColor; } set { redColor = value; } }
+
+	private static int randomness = 0;
+
+	/// <summary>
+	/// Boolean wrapper. Gets or sets the randomness. 0 = off, 1 = on.
+	/// </summary>
+	/// <value>The randomness.</value>
+	public int Randomness { get { return randomness; } set { randomness = value; } }
 
 
 	public static ConfigurationDTO Instance {
 		get {
-			return instance ?? (instance = new GameObject ("SingConfigurationDTOleton").AddComponent<ConfigurationDTO> ());
+			return instance ?? (instance = new GameObject ("SingletonConfigurationDTO").AddComponent<ConfigurationDTO> ());
 		}
 	}
-
-
-
-
-
-
 }
