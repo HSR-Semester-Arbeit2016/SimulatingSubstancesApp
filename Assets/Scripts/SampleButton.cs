5fe521cbcbc481ea52f54a9644d08d2aa4baa546
Roberto Cuervo <rcuervoa@hsr.ch>
2016-12-06 21:55:17 +0100

    Added Main2 scene in order to avoid the lost of the first click in the UI
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

---
+++ Assets/Scripts/SampleButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour {
+
+	public Button buttonComponent;
+	public Text nameLabel;
+
+
+	private Item item;
+	private ScrollList scrollList;
+
+	// Use this for initialization
+	void Start () 
+	{
+		buttonComponent.onClick.AddListener (OnButtonClick);
+	}
+
+	public void Setup(Item currentItem, ScrollList currentScrollList)
+	{
+		item = currentItem;
+		nameLabel.text = item.configurationName;
+		scrollList = currentScrollList;
+
+	}
+
+	public void OnButtonClick()
+	{
+		scrollList.TryTransferItemToOtherShop (item);
+		#if DEBUG
+		Debug.Log("Button: " + item.configurationName + "clicked!");
+		#endif
+	}
+}
