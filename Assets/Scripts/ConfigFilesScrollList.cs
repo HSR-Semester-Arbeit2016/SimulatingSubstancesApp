fe4596edb549d83a8d8a0fbf7c0eee582133606a
khoepli <khoepli@cybersystems.ch>
2016-12-09 07:02:16 +0100

    ConfigFilesScrollList refactored

--- Assets/Scripts/ConfigFilesScrollList.cs
+++ Assets/Scripts/ConfigFilesScrollList.cs
@@ -1,108 +1,95 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class ConfigFilesScrollList : MonoBehaviour
 {
-	public GameObject listButton;
-	public Transform contentPanel;
-	public System.Action<int> OnSelectionChanged;
-	private int index;
+    public static readonly List<string> defaultConfigurations = new List<string>
+    {
+        "Sober",
+        "Slightly Drunk",
+        "Drunk",
+        "Very Drunk",
+        "Create",
+        "Delete"
+    };
 
-	private int Index { get { return index; } set { this.index = value; } }
-
-	private List<ConfigFile> configFilesList;
-
-	private List<ConfigFile> ConfigFilesList { get { return configFilesList; } set { configFilesList = value; } }
-
-	private readonly static List<string> defaultConfigurations = new List<string> (new string[] {
-		"Sober",
-		"Slightly Drunk",
-		"Drunk",
-		"Very Drunk",
-		"Create",
-		"Delete"
-	});
-		
-	private readonly ReadOnlyCollection<string> readOnlyConfigurations = 
-		new ReadOnlyCollection<string> (defaultConfigurations);
-
-	public ConfigFile SelectedConfig { get; private set; }
-
-	private string applicationPath;
-
-	private string ApplicationPath { get { return applicationPath; } set { applicationPath = value; } }
-
-	void Start ()
-	{
-		ConfigFilesList = new  List<ConfigFile> ();
-		ApplicationPath = Application.persistentDataPath;
-		this.FillListWithDefaultConfigs ();
-		this.FillListWithSavedConfigFiles ();	
-		this.FillListInGui ();
-	}
+    private List<ConfigFile> configFilesList;
+    public Transform contentPanel;
+    public GameObject listButton;
+    public Action<int> OnSelectionChanged;
+    public ConfigFile SelectedConfig { get; private set; }
 
 
-	public ConfigFile this [int index] {
-		get { return configFilesList [index]; }
-	}
+    public ConfigFile this[int index]
+    {
+        get { return configFilesList[index]; }
+    }
 
-	public void RemoveSelectedConfig ()
-	{
-		foreach (var config in readOnlyConfigurations) {
-			if (config.Equals (SelectedConfig.FileName)) {
-				throw new TriedToDeleteDefaultConfigException ("The configuration you tried to delete\n is a default configuration!");
-			} 
-		}
-		ConfigFilesList.Remove (SelectedConfig);
-		this.ClearListInGui ();
-		this.FillListInGui ();
-	}
+    private void Start()
+    {
+        configFilesList = new List<ConfigFile>();
+        FillListWithDefaultConfigs();
+        FillListWithSavedConfigFiles();
+        FillListInGui();
+    }
 
-	private void FillListWithDefaultConfigs ()
-	{
-		foreach (var fileName in readOnlyConfigurations) {
-			ConfigFile newConfigFile = new ConfigFile (fileName, "", true);
-			configFilesList.Add (newConfigFile);
-		}
-	}
+    public void RemoveSelectedConfig()
+    {
+        foreach (var config in configFilesList)
+        {
+            if (config.Equals(SelectedConfig.FileName))
+            {
+                throw new TriedToDeleteDefaultConfigException(
+                    "The configuration you tried to delete\n is a default configuration!");
+            }
+        }
+        configFilesList.Remove(SelectedConfig);
+        ClearListInGui();
+        FillListInGui();
+    }
 
-	private void FillListWithSavedConfigFiles ()
-	{
-		string[] fileEntries = Directory.GetFiles (ApplicationPath);
-		foreach (string fileName in fileEntries) {
-			ConfigFile newConfigFile = new ConfigFile (Path.GetFileName (fileName), fileName, false);
-			this.configFilesList.Add (newConfigFile);
-		}
-	}
+    private void FillListWithDefaultConfigs()
+    {
+        foreach (var fileName in defaultConfigurations)
+        {
+            var newConfigFile = new ConfigFile(fileName, "", true);
+            configFilesList.Add(newConfigFile);
+        }
+    }
 
-	private void FillListInGui ()
-	{
-		foreach (var file in ConfigFilesList) {
-			GameObject newButton = Instantiate (listButton) as GameObject;
-			ListButton button = newButton.GetComponent<ListButton> ();
-			button.nameLabel.text = file.FileName;
-			int index = configFilesList.IndexOf (file);
-			button.button.onClick.AddListener (() => {
-				OnButtonClicked (index);
-			});
-			newButton.transform.SetParent (contentPanel);
-		}
-	}
+    private void FillListWithSavedConfigFiles()
+    {
+        var fileEntries = Directory.GetFiles(Application.persistentDataPath);
+        foreach (var fileName in fileEntries)
+        {
+            var newConfigFile = new ConfigFile(Path.GetFileName(fileName), fileName, false);
+            configFilesList.Add(newConfigFile);
+        }
+    }
 
-	private void ClearListInGui ()
-	{
-		contentPanel.DetachChildren ();
-	}
+    private void FillListInGui()
+    {
+        foreach (var file in configFilesList)
+        {
+            var newButton = Instantiate(listButton);
+            var button = newButton.GetComponent<ListButton>();
+            button.nameLabel.text = file.FileName;
+            var index = configFilesList.IndexOf(file);
+            button.button.onClick.AddListener(() => { OnButtonClicked(index); });
+            newButton.transform.SetParent(contentPanel);
+        }
+    }
 
-	private void OnButtonClicked (int index)
-	{
-		SelectedConfig = configFilesList [index];
-		OnSelectionChanged (index);
-		Index = index;
-	}
+    private void ClearListInGui()
+    {
+        contentPanel.DetachChildren();
+    }
+
+    private void OnButtonClicked(int index)
+    {
+        SelectedConfig = configFilesList[index];
+        OnSelectionChanged(index);
+    }
 }
