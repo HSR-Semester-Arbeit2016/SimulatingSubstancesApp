073257869786d9093e03f49cd7ee947017b72d74
Roberto Cuervo <rcuervoa@hsr.ch>
2016-12-18 13:52:56 +0100

    Removed Default Configuration from Delete Scene Configurations List

--- Assets/Scripts/ConfigFilesScrollList.cs
+++ Assets/Scripts/ConfigFilesScrollList.cs
@@ -6,82 +6,70 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class ConfigFilesScrollList : MonoBehaviour
-    {
-        private List<ConfigFile> configFilesList;
-        public Transform contentPanel;
-        public GameObject listButton;
-        public Action<int> OnSelectionChanged;
-        public ConfigFile SelectedConfig { get; private set; }
+	/// <summary>
+	/// In Unity there are no scrollable lists with selectable elements. This class provides that functionality by creating, 
+	/// filling and updating the <see cref="ConfigFile"/>list used in the <see cref="DeleteConfigViewModel"/>
+	/// </summary>
+	public class ConfigFilesScrollList : MonoBehaviour
+	{
+		private List<ConfigFile> configFilesList;
+		public Transform contentPanel;
+		public GameObject listButton;
+		public Action<int> OnSelectionChanged;
 
-        public ConfigFile this[int index]
-        {
-            get { return configFilesList[index]; }
-        }
+		public ConfigFile SelectedConfig { get; private set; }
 
-        private void Start()
-        {
-            configFilesList = new List<ConfigFile>();
-            FillListWithDefaultConfigs();
-            FillListWithSavedConfigFiles();
-            FillListInGui();
-        }
+		public ConfigFile this [int index] {
+			get { return configFilesList [index]; }
+		}
 
-        public void RemoveSelectedConfig()
-        {
-            foreach (var config in DefaultConfigurations.List)
-            {
-                if (config.Equals(SelectedConfig.FileName))
-                {
-                    throw new TriedToDeleteDefaultConfigException(Messages.ConfigDeletingDefaultError);
-                }
-            }
-            configFilesList.Remove(SelectedConfig);
-            ClearListInGui();
-            FillListInGui();
-        }
+		private void Start ()
+		{
+			configFilesList = new List<ConfigFile> ();
+			FillListWithSavedConfigFiles ();
+			FillListInGui ();
+		}
 
-        private void FillListWithDefaultConfigs()
-        {
-            foreach (var fileName in DefaultConfigurations.List)
-            {
-                var newConfigFile = new ConfigFile(fileName, "");
-                configFilesList.Add(newConfigFile);
-            }
-        }
+		public void RemoveSelectedConfig ()
+		{
+			configFilesList.Remove (SelectedConfig);
+			ClearListInGui ();
+			FillListInGui ();
+		}
 
-        private void FillListWithSavedConfigFiles()
-        {
-            var fileEntries = Directory.GetFiles(Application.persistentDataPath);
-            foreach (var fileName in fileEntries)
-            {
-                var newConfigFile = new ConfigFile(Path.GetFileName(fileName), fileName);
-                configFilesList.Add(newConfigFile);
-            }
-        }
 
-        private void FillListInGui()
-        {
-            foreach (var file in configFilesList)
-            {
-                var newButton = Instantiate(listButton);
-                var button = newButton.GetComponent<ListButton>();
-                button.nameLabel.text = file.FileName;
-                var index = configFilesList.IndexOf(file);
-                button.button.onClick.AddListener(() => { OnButtonClicked(index); });
-                newButton.transform.SetParent(contentPanel);
-            }
-        }
+		private void FillListWithSavedConfigFiles ()
+		{
+			var fileEntries = Directory.GetFiles (Application.persistentDataPath);
+			foreach (var fileName in fileEntries) {
+				var newConfigFile = new ConfigFile (Path.GetFileName (fileName), fileName);
+				configFilesList.Add (newConfigFile);
+			}
+		}
 
-        private void ClearListInGui()
-        {
-            contentPanel.DetachChildren();
-        }
+		private void FillListInGui ()
+		{
+			foreach (var file in configFilesList) {
+				var newButton = Instantiate (listButton);
+				var button = newButton.GetComponent<ListButton> ();
+				button.nameLabel.text = file.FileName;
+				var index = configFilesList.IndexOf (file);
+				button.button.onClick.AddListener (() => {
+					OnButtonClicked (index);
+				});
+				newButton.transform.SetParent (contentPanel);
+			}
+		}
 
-        private void OnButtonClicked(int index)
-        {
-            SelectedConfig = configFilesList[index];
-            OnSelectionChanged(index);
-        }
-    }
+		private void ClearListInGui ()
+		{
+			contentPanel.DetachChildren ();
+		}
+
+		private void OnButtonClicked (int index)
+		{
+			SelectedConfig = configFilesList [index];
+			OnSelectionChanged (index);
+		}
+	}
 }
